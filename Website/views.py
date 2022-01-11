abf9365b02ca313b670ecc540d41099641cb58fc
bogdanmihalache22 <bogdan.mihalachept@gmail.com>
2022-01-11 14:09:10 +0200

    third commit

--- Website/views.py
+++ Website/views.py
@@ -1,30 +1,32 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash
+from flask.helpers import flash
+from flask.json import jsonify
 from flask_login import login_required, current_user
+from werkzeug.urls import url_encode_stream
 from .models import Note
 from . import db
 import json
 
+
+
 views = Blueprint('views', __name__)
 
-
-@views.route('/', methods=['GET', 'POST'])
+@views.route('/', methods = ['GET', 'POST'])
 @login_required
 def home():
     if request.method == 'POST':
         note = request.form.get('note')
 
         if len(note) < 1:
-            flash('Note is too short!', category='error')
+            flash('Note is to short!', category='error')
         else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
+            new_Note = Note(data=Note, user_id = current_user.id)
+            db.session.add(new_Note)
             db.session.commit()
-            flash('Note added!', category='success')
+            flash('Note added!', category='succes')
+    return render_template("home.html", user =  current_user)
 
-    return render_template("home.html", user=current_user)
-
-
-@views.route('/delete-note', methods=['POST'])
+@views.route('/delete-note', methods = ['POST'])
 def delete_note():
     note = json.loads(request.data)
     noteId = note['noteId']
@@ -32,6 +34,6 @@ def delete_note():
     if note:
         if note.user_id == current_user.id:
             db.session.delete(note)
-            db.session.commit()
-
-    return jsonify({})
+            db.session.commit
+            
+        return jsonify({})
